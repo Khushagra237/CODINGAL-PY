4da2748858ca3e9e57382f7d3bfd9523bce5185e
Khushagra237 <148641541+Khushagra237@users.noreply.github.com>
2025-03-28 17:42:18 +0530

    string

---
+++ ls 2/ls 2.1/ls 3/strng.py/ls 4]/ls 6/ls 7/for.py
@@ -0,0 +1,7 @@
+string = input("Please enter your own string: ")
+string2= ('')
+for i in string:
+    string2 = i + string2
+
+print("\nTheOriginal String = " , string)
+print("\nThe Reversed String = " , string2)    
